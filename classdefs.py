edb6c1d8988386c1c73b782c75dbad46dd713670
Nikhil Pinnaparaju <nikhilpinnaparaju@gmail.com>
2017-08-20 12:09:29 +0530

    Trial Timer

--- classdefs.py
+++ classdefs.py
@@ -1,13 +1,18 @@
 class person():
     def __init__(self):
         self.life=1
-        self.location= {'x':1,'y':1}
 
 class bomberman(person):
     def __init__(self):
         person.__init__(self)
         self.life=3
+        self.location= {'x':1,'y':1}
+        self.bombs_left = 1
 
 class bomb():
     def __init__(self):
         self.time_left = 3
+
+class enemy():
+    def __init__(self):
+        person.__init__(self)
--- inp.py
+++ inp.py
@@ -1,51 +1,74 @@
 from getch import getch
 import os
 from grid import *
+import datetime as timer
+import time
+import sys,select
 
 def controller(bomber):
     while 1:
-        control = getch()
+        total_time = 0
+        start = timer.datetime.now()
+        stop = start
 
-        if control=='q':
+        while ((stop - start).microseconds < 10000):
+            control = getch()
+
+            sys.stdout.flush()
+
+            if control=='q':
+                break
+            else:
+                if control=='d':
+                    if grid[bomber.location['x']][bomber.location['y']+1] == ' ':
+                        if grid[bomber.location['x']][bomber.location['y']] == 'B':
+                            grid[bomber.location['x']][bomber.location['y']] = ' '
+
+                        grid[bomber.location['x']][bomber.location['y']+1] = 'B'
+                        bomber.location['y'] = bomber.location['y']+1
+
+                if control=='w':
+                    if grid[bomber.location['x']-1][bomber.location['y']]==' ':
+                        if grid[bomber.location['x']][bomber.location['y']] == 'B':
+                            grid[bomber.location['x']][bomber.location['y']] = ' '
+                        
+                        grid[bomber.location['x']-1][bomber.location['y']] = 'B'
+                        bomber.location['x'] = bomber.location['x']-1
+
+                if control=='a':
+                    if grid[bomber.location['x']][bomber.location['y']-1] == ' ':
+                        if grid[bomber.location['x']][bomber.location['y']] == 'B':
+                            grid[bomber.location['x']][bomber.location['y']] = ' '
+                        
+                        grid[bomber.location['x']][bomber.location['y']-1] = 'B'
+                        bomber.location['y'] = bomber.location['y']-1
+
+                if control=='s':
+                    if grid[bomber.location['x']+1][bomber.location['y']] == ' ':
+                        
+                        if grid[bomber.location['x']][bomber.location['y']] == 'B':
+                            grid[bomber.location['x']][bomber.location['y']] = ' '
+                        
+                        grid[bomber.location['x']+1][bomber.location['y']] = 'B'
+                        bomber.location['x'] = bomber.location['x']+1
+
+                if control=='b':
+                    if bomber.bombs_left:
+                        grid[bomber.location['x']][bomber.location['y']] = str(bomber.bomb.time_left)
+                        bomber.bombs_left = bomber.bombs_left - 1
+
+            stop = timer.datetime.now()
+
+        if control == 'q':
             break
-        else:
-            if control=='d':
-                if grid[bomber.location['x']][bomber.location['y']+1] == ' ':
-                    if grid[bomber.location['x']][bomber.location['y']] == 'B':
-                        grid[bomber.location['x']][bomber.location['y']] = ' '
+        
+        total_time = total_time + 0.1
+        print("total time is ",total_time)
 
-                    grid[bomber.location['x']][bomber.location['y']+1] = 'B'
-                    bomber.location['y'] = bomber.location['y']+1
+        if (total_time-1 == 0):
+            bomber.bomb.time_left = bomber.bomb.time_left - 1
 
-            if control=='w':
-                if grid[bomber.location['x']-1][bomber.location['y']]==' ':
-                    if grid[bomber.location['x']][bomber.location['y']] == 'B':
-                        grid[bomber.location['x']][bomber.location['y']] = ' '
-                    
-                    grid[bomber.location['x']-1][bomber.location['y']] = 'B'
-                    bomber.location['x'] = bomber.location['x']-1
+        os.system('clear')
+        print_board(grid)
 
-            if control=='a':
-                if grid[bomber.location['x']][bomber.location['y']-1] == ' ':
-                    if grid[bomber.location['x']][bomber.location['y']] == 'B':
-                        grid[bomber.location['x']][bomber.location['y']] = ' '
-                    
-                    grid[bomber.location['x']][bomber.location['y']-1] = 'B'
-                    bomber.location['y'] = bomber.location['y']-1
-
-            if control=='s':
-                if grid[bomber.location['x']+1][bomber.location['y']] == ' ':
-                    
-                    if grid[bomber.location['x']][bomber.location['y']] == 'B':
-                        grid[bomber.location['x']][bomber.location['y']] = ' '
-                    
-                    grid[bomber.location['x']+1][bomber.location['y']] = 'B'
-                    bomber.location['x'] = bomber.location['x']+1
-
-            if control=='b':
-                grid[bomber.location['x']][bomber.location['y']] = str(bomber.bomb.time_left)
-
-            os.system('clear')
-            print_board(grid)
-
-            print("you moved:",control)
+        print("you moved:",control)
