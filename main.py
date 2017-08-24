cd17e24291ac204cbde8c7183f346851949b7a2e
Nikhil Pinnaparaju <nikhilpinnaparaju@gmail.com>
2017-08-25 03:01:32 +0530

    Need to do PEP8 on main but people die

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from multiprocessing import Process
 from threader import *
 import random
 import os
+import copy
 
 grid[1][1] = 'B'
 
@@ -39,33 +40,33 @@ class killingFunction(RepeatedTimer):
         RepeatedTimer.__init__(self, 1, self.kill)
 
     def kill(self):
-        repl = self.enemies
+        repl = set()
+
+        for i in enemies:
+            repl.add(i)
+
         bomber = self.bomber
 
         if (bomber.bomb.location['x'] is not None):
             for enemy in repl:
-                if ((grid[enemy.location['x']] -
-                     grid[bomber.bomb.location['x']]) -
-                        (grid[enemy.location['y']] -
-                            grid[bomber.bomb.location['y']]) < 2):
-                    grid[enemy.location['x']][enemy.location['y']] = " "
-                    enemy.stop()
-                    enemies.discard(enemy)
+                if (bomber.bomb.time_left == -1):
+                    if ((enemy.location['x'] - bomber.bomb.location['x']) - (enemy.location['y'] - bomber.bomb.location['y']) < 2):
+                        grid[enemy.location['x']][enemy.location['y']] = " "
+                        enemy.stop()
+                        enemies.discard(enemy)
 
         if (bomber.bomb.location['x'] is not None):
-            if (((grid[bomber.location['x']] -
-                 grid[bomber.bomb.location['x']]) -
-                (grid[bomberman.location['y']] -
-                    grid[bomber.bomb.location['y']]) < 2) and bomber.life):
-                bomber.life = bomber.life - 1
-                grid[bomber.location['x']][bomber.location['y']] = " "
-                bomber.location['x'] = 1
-                bomber.location['y'] = 1
-                grid[1][1] = "B"
+            if (bomber.bomb.time_left == -1):
+                if (((bomber.location['x'] - bomber.bomb.location['x']) - (bomber.location['y'] - bomber.bomb.location['y']) < 2) and bomber.life):
+                    bomber.life = bomber.life - 1
+                    grid[bomber.location['x']][bomber.location['y']] = " "
+                    bomber.location['x'] = 1
+                    bomber.location['y'] = 1
+                    grid[1][1] = "B"
 
-            elif (bomber.life == 0):
-                printf("Game Over")
-                os._exit(1)
+                elif (bomber.life == 0):
+                    printf("Game Over")
+                    os._exit(1)
 
 scoring = killingFunction()
 scoring.start()
